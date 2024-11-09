2746f0074d142cb4aadcd9bc981860af88a06121
Karina Quiroga <iquiroga5@estudiantes.areandina.edu.co>
2024-11-09 17:47:17 -0500

    Tablas de comunidades indigenas y consumidores

--- apps/deforestation/admin.py
+++ apps/deforestation/admin.py
@@ -4,9 +4,13 @@ from .models.governments import Governments
 from .models.companies import Companies
 from .models.real_state_developers import RealEstateDeveloper
 from .models.tourist import Tourist
+from .models.consumers import Consumers
+from .models.indigenous_communities import IndigenousCommunities
 # Register your models here.
 admin.site.register(AmbientalistOng)
 admin.site.register(Governments)
 admin.site.register(Companies)
 admin.site.register(RealEstateDeveloper)
 admin.site.register(Tourist)
+admin.site.register(Consumers)
+admin.site.register(IndigenousCommunities)
